7ed3e657e0ec4af42283fdb00dced3918d5abd03
Francesco-Matteucci <francescomatteucci31@gmail.com>
2024-09-16 16:39:28 +0200

    add dimanic AppFooter

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,90 +3,75 @@ export default {
   data() {
     return {
       name: 'AppFooter',
+      footerSections: [
+        {
+          title: 'DC COMICS',
+          items: ['Characters', 'Comics', 'Movies', 'TV', 'Games', 'Videos', 'News']
+        },
+        {
+          title: 'SHOP',
+          items: ['Shop DC', 'Shop DC Collectibles']
+        },
+        {
+          title: 'DC',
+          items: ['Terms Of Use', 'Privacy policy (New)', 'Ad Choices', 'Advertising', 'Jobs', 'Subscriptions', 'Talent Workshops', 'CPSC Certificates', 'Ratings', 'Shop Help', 'Contact Us']
+        },
+        {
+          title: 'SITES',
+          items: ['DC', 'MAD Magazine', 'DC Kids', 'DC Universe', 'DC Power Visa']
+        }
+      ]
     }
   }
 }
 </script>
 
 <template>
-    <footer>
-      <div class="top-footer">
-        <div class="d-flex container">
-          <div class="col-md-8 footer-lists">
-            <div class="row">
-              <div class="col">
-                <h4>DC COMICS</h4>
-                <ul>
-                  <li>Characters</li>
-                  <li>Comics</li>
-                  <li>Movies</li>
-                  <li>TV</li>
-                  <li>Games</li>
-                  <li>Videos</li>
-                  <li>News</li>
-                </ul>
-                <h4>SHOP</h4>
-                <ul>
-                  <li>Shop DC</li>
-                  <li>Shop DC Collectibles</li>
-                </ul>
-              </div>
-              <div class="col">
-                <h4>DC</h4>
-                <ul>
-                  <li>Terms Of Use</li>
-                  <li>Privacy policy (New)</li>
-                  <li>Ad Choices</li>
-                  <li>Advertising</li>
-                  <li>Jobs</li>
-                  <li>Subscriptions</li>
-                  <li>Talent Workshops</li>
-                  <li>CPSC Certificates</li>
-                  <li>Ratings</li>
-                  <li>Shop Help</li>
-                  <li>Contact Us</li>
-                </ul>
-              </div>
-              <div class="col">
-                <h4>SITES</h4>
-                <ul>
-                  <li>DC</li>
-                  <li>MAD Magazine</li>
-                  <li>DC Kids</li>
-                  <li>DC Universe</li>
-                  <li>DC Power Visa</li>
-                </ul>
-              </div>
+  <footer>
+    <div class="top-footer">
+      <div class="d-flex container">
+        <div class="col-md-8 footer-lists">
+          <div class="row">
+            <div v-for="(section, index) in footerSections" :key="index" class="col">
+              <h4>{{ section.title }}</h4>
+              <ul>
+                <li v-for="(item, idx) in section.items" :key="idx">{{ item }}</li>
+              </ul>
             </div>
           </div>
-          <div class="col-md-4 footer-image text-end">
-            <img src="../assets/img/dc-logo-bg.png" alt="DC Image">
-          </div>
+        </div>
+        <div class="col-md-4 footer-image text-end">
+          <img src="../assets/img/dc-logo-bg.png" alt="DC Image">
         </div>
       </div>
-      <div class="bottom-footer container d-flex justify-content-between">
-        <div class="sign-up text-center my-3">
-          <button class="btn btn-primary">SIGN-UP NOW!</button>
-        </div>
-  
-        <div class="social-media d-flex align-items-center gap-4">
-          <p class="mb-0">FOLLOW US</p>
-          <div class="social-icons d-flex justify-content-center gap-3">
-            <img src="../assets/img/footer-facebook.png" alt="Facebook">
-            <img src="../assets/img/footer-twitter.png" alt="Twitter">
-            <img src="../assets/img/footer-youtube.png" alt="Youtube">
-            <img src="../assets/img/footer-pinterest.png" alt="Pinterest">
-            <img src="../assets/img/footer-periscope.png" alt="Periscope">
-          </div>
+    </div>
+    <div class="bottom-footer container d-flex justify-content-between">
+      <div class="sign-up text-center my-3">
+        <button class="btn btn-primary">SIGN-UP NOW!</button>
+      </div>
+
+      <div class="social-media d-flex align-items-center gap-4">
+        <p class="mb-0">FOLLOW US</p>
+        <div class="social-icons d-flex justify-content-center gap-3">
+          <img src="../assets/img/footer-facebook.png" alt="Facebook">
+          <img src="../assets/img/footer-twitter.png" alt="Twitter">
+          <img src="../assets/img/footer-youtube.png" alt="Youtube">
+          <img src="../assets/img/footer-pinterest.png" alt="Pinterest">
+          <img src="../assets/img/footer-periscope.png" alt="Periscope">
         </div>
       </div>
-    </footer>
-  </template>
+    </div>
+  </footer>
+</template>
   
   <style scoped lang="scss">
-  footer {
-    background-color: #222;
-    color: #fff;
+    footer {
+        background-color: #222;
+        color: #fff;
+    }
+
+  .bottom-footer {
+    color: #007bff;
   }
   
   .top-footer {
